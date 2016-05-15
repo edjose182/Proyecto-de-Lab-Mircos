dfba41b8fb7ad0146757a720fe490ba0442e06b0
Edgar Campos <edjose2206@gmail.com>
2016-05-15 11:19:56 -0600

    Protocolo de comunicación
    Se agrego un protocolo de comunicación de forma que los datos que son
    enviados por matlab puedan  ser procesados por el arduino.

--- Progras/Control_Motor.ino
+++ Progras/Control_Motor.ino
@@ -1,47 +1,60 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: Instituto Tecnológico de Costa Rica
-// Engineers: Edgar Campos, Edgar Solera y José Netzer 
-// 
-// Create Date:     23/03/2016 
-// Module Name:    Control_Motor
-// Description: Este codigo se encarga de controlar la velocidad y sentido de rotación del motor
-// esto mediante un envio de datos desde matlab hasta el arduino.
-//////////////////////////////////////////////////////////////////////////////////
-
 #include "DualMC33926MotorShield.h"  ///Primero se habilitan las librerias
 
 DualMC33926MotorShield md; ///Se habilita la variable md
-int M1PWM =9;              ///Se indican los pines
-int M1DIR=7;               ///Que se activaran en el arduino 
-//int M1FB=A0;
-int D2n=4;
-int SFn=12;
 
 void setup()
 {
-  pinMode(D2n,OUTPUT);    ////Se habilitan los pines de salida
-  pinMode(M1PWM,OUTPUT);  ////Se habilitan los pines de salida
-  pinMode(M1DIR,OUTPUT);  ////Se habilitan los pines de salida
   Serial.begin(9600);     ///Se habilita el puerto serial
-  Serial.setTimeout(100); ///Se pone un tiempo de espera para la recepción de datos
   md.init();              ///se inicializa la variable md
 }
+//Analog read pins
+const int xPin=10;
+
+//The minimum and maximum values that came from
+//the accelerometer while standing still
+//You very well may need to change these
+int minVal = 265;
+int maxVal = 402;
+char cadena[30]; //Creamos un array que almacenará los caracteres que escribiremos en la consola del PC. Le asignamos  un tope de caracteres, en este caso 30
+byte posicion=0;  //Variable para cambiar la posición de los caracteres del array
+int valor;  //Variable del valor entero
+
+int ON=1;
+int OFF=2;
+
+//to hold the caculated values
+double x;
 
 void loop() {
   /*
-   * Evaluamos el momento en el cual recibimos un caracter
+   * os el momento en el cual recibimos un caracter
    * a través del puerto serie
    */
 
+  int xRead= analogRead(xPin);
+
+  x=xRead;
+
   if (Serial.available() > 0) {
 
-    
-    int velocidad = Serial.readString().toInt();   ////Se convierte el dato recibido en un valor entero
+              Serial.println(ON);
 
+              memset(cadena, 0,sizeof(cadena));//memset borra el contenido del array  "cadena" desde la posición 0 hasta el final sizeof
+              while(Serial.available()>0) //Mientras haya datos en el buffer ejecuta la función
+              {
+                delay(5); //Poner un pequeño delay para mejorar la recepción de datos
+                cadena[posicion]=Serial.read();//Lee un carácter del string "cadena" de la "posicion", luego lee el siguiente carácter con "posicion++"
+                posicion++;
+              }
+              
+              valor=atoi(cadena);//Convertimos la cadena de caracteres en enteros
+              md.setM1Speed(valor);
 
-    md.setM1Speed(velocidad); //Se regula la velocidad del motor introduciendo un valor de corriente que esta entre -400 y 400
-                              //El dato "velocidad" es la velocidad procesada en matlab pero escalada para que este en un rango
-                              //Entre -400 y 400
-    
+              
+              Serial.println(x);
+              delay(700);
+              Serial.println(OFF);
+              posicion=0;//Ponemos la posicion a 0
    }
 }
+
